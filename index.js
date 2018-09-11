0c560b3b2e36669bcd166ce133e6a8519e7aac6d
Terrin Day <terrinday@gmail.com>
2018-09-11 22:58:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // write your code below!
 function happyHolidays()
 {
+<<<<<<< HEAD
 return "Happy holidays!" 
 }
 
@@ -17,4 +18,11 @@ function happyHolidayTo(holiday, name)
 function holidayCountdown(holiday,days)
 {
   return `It's ${days} days until ${holiday}!`
+=======
+return "Happy holidays!" }
+
+function happyHolidaysTo(name) 
+{
+  return `Happy holidays ${name}!`
+>>>>>>> 8328d854fd089babba6bec6deacd1adc520c56e1
 }
